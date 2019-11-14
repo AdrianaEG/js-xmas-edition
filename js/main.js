4ad63740126a9655f4208635af3a4946775a6079
AdrianaEG <adriana.ngc438@gmail.com>
2019-11-13 21:58:56 -0300

    Agrega tareas de la clase 7 y algunas explicaciones de la clase.

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//SINTÁXIS: ACCEDER A ELEMENTOS DE UN FORMULARIO A TRAVÉS DE SUS ATRIBUTOS (NO POR ID O CLASES)
+
+//POR EJEMPLO PUEDE SABER QUE RADIOBUTTOM ESTÁ SELECCIONADO 
+
+//console.log(document.querySelector('[name=nombre]')); //le estoy diciendo dame los elementos que tengan el atributo name='nombre' :)
+
+//console.log(document.querySelectorAll('[name=comportamiento]')); // esto me trae todos los radiobutomm que tiene el atributo name='comportamiento'; 
+
+//console.log(document.querySelector('#carta-a-santa')); // me devuelve el formulario que tiene el id ese.
+//console.log(document.querySelector('#carta-a-santa').comportamiento); //me devuelve los elementos que tienen como name comportamiento y están dentro del form con id carta-a-santa. 
+//console.log(document.querySelector('#carta-a-santa').comportamiento.value); //me devuelve el radiobuttom que está seleccionado. 
+//console.log(document.querySelector('#carta-a-santa').nombre.value); //lo que está haciendo es devolver el valor que está dentro del input con name nombre que está dentro del form... es decir accedo a los datos a traves de su atributo no a través del id o clases como veniamos haciendo. 
+//console.log(document.formulario)//me trae el formulario que tiene como name='formulario'.
+//console.log(document.forms['carta-a-santa']); // es otra forma de hacer lo mismo. 
+
+const $formulario = document.querySelector('#carta-a-santa');//otra forma: document.formulario; es decir agarrarlo por el name... otra es document.querySelector('[name=formulario]');
+
+const nombre = $formulario.nombre.value;// otra forma document.querySelector('[name=nombre]').value;
+
+const ciudad = document.querySelector('[name=ciudad]').value;//otra forma: document.formulario.ciudad.value; o const $ciudad = $formulario.ciudad.value;
+
+const comportamiento = $formulario.comportamiento.value;//document.querySelectorAll('[name=comportamiento]').value;
+
+const descripcionRegalo = $formulario['descripcion-regalo'].value;//escribí el name entre llaves y con comillas porque tiene un guion en el medio :)
+//otra forma de hacerlo sería esta: document.querySelector('[name=descripcion-regalo]').value;
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcionRegalo);
+
+
+function validarNombre(nombre){
+    nombre = nombre.trim();//el .trim() es para eliminar espacios en ambos extremos.
+    /* Una forma: 
+    if(nombre === ''){
+        console.log('Este campo debe tener al menos 1 caracter');
+    }
+    else if(nombre.length > 50){
+        console.log('Este campo debe tener menos de 50 caracteres');
+    }*/
+    if(nombre.length===0){
+       return 'Este campo debe tener al menos 1 caracter'; 
+    }
+    else if(nombre.length>=50){
+       return 'Este campo debe tener menos de 50 caracteres'; 
+    }
+    return '';
+}
+validarNombre(nombre);
+
+function validarCiudad(ciudad){
+    if(ciudad === ""){
+        return 'Debe seleccionar una ciudad';
+    }
+    return '';
+}
+console.log(validarCiudad(ciudad));
+
+function validarDescripcionRegalo(descripcionRegalo){
+    descripcionRegalo = descripcionRegalo.trim();
+    if(descripcionRegalo.length === 0){
+        return 'Debe escribir una descripción del regalo'
+    }
+    return '';
+}
+console.log(validarDescripcionRegalo(descripcionRegalo));
--- js/pruebas.js
+++ js/pruebas.js
@@ -13,3 +13,34 @@ function probarValidarNombre() {
 }
 
 probarValidarNombre();
+
+//console.assert es la función que vamos a utilizar para hacer pruebas. si yo hago console.assert(1===1)--> undefined. console.assert(1===2)--> assertion failed.
+//console.assert(1===2. "1 no es igual a 2");
+
+/* ejemplo de otra prueba. 
+function sumar(a, b){
+    return a+b;
+}
+
+console.assert(sumar(1, 2) === 3, "la suma de 1 + 2 no dio 3");//si no funcionó me da ese mensaje... sino no me manda nada. */
+
+//ahora validarCiudad() y validarDescripcionRegalo() no sean vacíos y hacer las pruebas de eso. 
+
+//crear también pruebas para las funciones de las tareas 5 y 6. 
+
+
+function probarValidarCiudad(){
+    console.assert(
+        validarCiudad('')==='Debe seleccionar una ciudad', 'validarCiudad() No validó si no selecciona una ciudad'
+    );
+}
+probarValidarCiudad();
+
+function probarValidarDescripcionRegalo(){
+    console.assert(
+        validarDescripcionRegalo('') === 'Debe escribir una descripción del regalo', 'validarDescripcionRegalo() no validó si no escribe descripción'
+    );
+}
+
+probarValidarDescripcionRegalo();
+
